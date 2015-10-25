3c74c7006564be26c69af62962f502e29a91b5a7
shunlin <shunlinliang@gmail.com>
2015-10-25 17:39:50 -0400

    Finished register page

--- public/assignment/register/register.controller.js
+++ public/assignment/register/register.controller.js
@@ -5,6 +5,22 @@
         .module("FormBuilderApp")
         .controller("RegisterController", RegisterController);
 
-    function RegisterController() {
+    function RegisterController($scope, $rootScope, $location, UserService) {
+        $scope.$location = $location;
+        $scope.register = register;
+        $scope.user = {};
+
+        function register() {
+            if ($scope.user.password != $scope.user.password2) alert("Password not match!");
+            else {
+                UserService.createUser(
+                    $scope.user,
+                    function(user) {
+                        $rootScope.loginUser = user;
+                        $location.url('/profile');
+                    }
+                )
+            }
+        }
     }
 })();
