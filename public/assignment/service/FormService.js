8dd40a51e6ee193c03477ecb67432387be140e43
shunlin <shunlinliang@gmail.com>
2015-10-25 22:00:07 -0400

    Fixed typo in guid function

--- public/assignment/service/FormService.js
+++ public/assignment/service/FormService.js
@@ -54,7 +54,7 @@
         }
 
         function guid() {
-            function S4() {
+            function s4() {
                 return Math.floor((1 + Math.random()) * 0x10000)
                     .toString(16)
                     .substring(1);
