e0dd078b7ea6108abd9af5d4e746a02451494f14
shunlin <shunlinliang@gmail.com>
2015-11-12 16:44:33 -0500

    Finished user service for server

---
+++ public/assignment/server/services/user.service.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = function(app, model) {
+    app.post("/api/assignment/user", function(req, res) {
+        res.json(model.create(req.body));
+    });
+
+    app.get("/api/assignment/user", function(req, res) {
+        var username = req.param("username");
+        var password = req.param("password");
+        if (username == null && password == null) {
+            res.json(model.findAll());
+            return;
+        }
+        if (password == null) {
+            res.json(model.findUserByUsername(username));
+            return;
+        }
+        var cred = {
+            username: username,
+            password: password
+        };
+        res.json(model.findUserByCredentials(cred));
+
+    });
+
+    app.get("/api/assignment/user/:id", function(req, res) {
+        res.json(model.findById(req.params.id));
+    });
+
+    app.put("/api/assignment/user/:id", function(req, res) {
+        res.json(model.updateUser(req.body));
+    });
+
+    app.delete("/api/assignment/user/:id", function(req, res) {
+        res.json(model.deleteUser(req.params.id));
+    });
+
+}
