13ff43173d3d40e04ce736784db81793d7ae0b48
shunlin <shunlinliang@gmail.com>
2015-11-21 21:41:20 -0500

    Use new template for final project; Refactor folder structure.

--- public/project/client/config.js
+++ public/project/client/config.js
@@ -8,49 +8,49 @@
     function Configure($routeProvider) {
         $routeProvider
             .when("/home",{
-                templateUrl: "home/home.view.html"
+                templateUrl: "views/home/home.view.html"
             })
             .when("/login", {
-                templateUrl: "login/login.view.html",
+                templateUrl: "views/login/login.view.html",
             })
             .when("/register", {
-                templateUrl: "register/register.view.html",
+                templateUrl: "views/register/register.view.html",
             })
             .when("/profile", {
-                templateUrl: "profile/profile.view.html",
+                templateUrl: "views/profile/profile.view.html",
             })
             .when("/search", {
-                templateUrl: "search/search.view.html"
+                templateUrl: "views/search/search.view.html"
             })
             .when("/search/:keyword", {
-                templateUrl: "search/search.view.html"
+                templateUrl: "views/search/search.view.html"
             })
             .when("/book/:id", {
-                templateUrl: "book/book.view.html"
+                templateUrl: "views/book/book.view.html"
             })
             .when("/bookEdit/:id", {
-                templateUrl: "bookEdit/bookEdit.view.html"
+                templateUrl: "views/bookEdit/bookEdit.view.html"
             })
             .when("/cart", {
-                templateUrl: "cart/cart.view.html"
+                templateUrl: "views/cart/cart.view.html"
             })
             .when("/checkout", {
-                templateUrl: "checkout/checkout.view.html"
+                templateUrl: "views/checkout/checkout.view.html"
             })
             .when("/orders", {
-                templateUrl: "orders/orders.view.html"
+                templateUrl: "views/orders/orders.view.html"
             })
             .when("/order/:id", {
-                templateUrl: "order/order.view.html"
+                templateUrl: "views/order/order.view.html"
             })
             .when("/allOrders", {
-                templateUrl: "allOrders/allOrders.view.html"
+                templateUrl: "views/allOrders/allOrders.view.html"
             })
             .when("/addBook", {
-                templateUrl: "addBook/addBook.view.html"
+                templateUrl: "views/addBook/addBook.view.html"
             })
             .when("/statistics", {
-                templateUrl: "statistics/statistics.view.html",
+                templateUrl: "views/statistics/statistics.view.html",
                 controller: "StatisticsController"
             })
             .otherwise({
