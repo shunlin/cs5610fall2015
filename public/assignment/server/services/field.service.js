d367abb4b2d6843a0b3d39a44a8b77913f5ea7df
shunlin <shunlinliang@gmail.com>
2015-11-29 17:47:19 -0500

    Updated field service for server

--- public/assignment/server/services/field.service.js
+++ public/assignment/server/services/field.service.js
@@ -1,23 +1,39 @@
 "use strict";
 
 module.exports = function(app, model) {
-    app.get("/api/assignment/form/:formId/field", function(req, res) {
-        res.json(model.findById(req.params.formId).fields);
-    });
+    app.get("/api/assignment/form/:formId/field", findFieldsById);
+    app.get("/api/assignment/form/:formId/field/:fieldId", findFieldById);
+    app.delete("/api/assignment/form/:formId/field/:fieldId", deleteField);
+    app.post("/api/assignment/form/:formId/field", createField);
+    app.put("/api/assignment/form/:formId/field/:fieldId", updateField);
 
-    app.get("/api/assignment/form/:formId/field/:fieldId", function(req, res) {
-        res.json(model.findFieldByFieldId(req.params.formId, req.params.fieldId));
-    });
+    function findFieldsById(req, res) {
+        model.findById(req.params.formId).then(function(form) {
+            res.json(form.fields);
+        });
+    }
 
-    app.delete("/api/assignment/form/:formId/field/:fieldId", function(req, res) {
-        res.json(model.deleteFieldFromForm(req.params.formId, req.params.fieldId));
-    });
+    function findFieldById(req, res) {
+        model.findFieldByFieldId(req.params.formId, req.params.fieldId).then(function(field) {
+            res.json(field);
+        })
+    }
 
-    app.post("/api/assignment/form/:formId/field", function(req, res) {
-        res.json(model.createFieldForForm(req.params.formId, req.body));
-    });
+    function deleteField(req, res) {
+        model.deleteFieldFromForm(req.params.formId, req.params.fieldId).then(function(fields) {
+            res.json(fields);
+        })
+    }
 
-    app.put("/api/assignment/form/:formId/field/:fieldId", function(req, res) {
-        res.json(model.updateFieldForForm(req.params.formId, req.params.fieldId, req.body));
-    });
-}
+    function createField(req, res) {
+        model.createFieldForForm(req.params.formId, req.body).then(function(field) {
+            res.json(field);
+        })
+    }
+
+    function updateField(req, res) {
+        model.updateFieldForForm(req.params.formId, req.params.fieldId, req.body).then(function(field) {
+            res.json(field);
+        });
+    }
+};
